6bcb6998ce1d6251a73a9e3b8001f60ff4336d54
Adrian Yong <11637545+ayong28@users.noreply.github.com>
2021-09-03 16:09:04 +1000

    Initial commit

---
+++ src/features/products/Products.tsx
@@ -0,0 +1,47 @@
+import React, { useEffect } from "react";
+import { useAppSelector, useAppDispatch } from "../../app/hooks";
+import { receivedProducts } from "./productSlice";
+import { getProducts } from "../../app/api";
+import styles from "./Products.module.css";
+import { addToCart } from "../cart/cartSlice";
+
+export function Products() {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    getProducts().then((products) => {
+      dispatch(receivedProducts(products));
+    });
+  }, []);
+
+  const products = useAppSelector((state) => state.products.products);
+  return (
+    <main className="page">
+      <ul className={styles.products}>
+        {Object.values(products).map((product) => (
+          <li key={product.id}>
+            <article className={styles.product}>
+              <figure>
+                <img src={product.imageURL} alt={product.imageAlt} />
+                <figcaption className={styles.caption}>
+                  {product.imageCredit}
+                </figcaption>
+              </figure>
+              <div>
+                <h1>{product.name}</h1>
+                <p>{product.description}</p>
+                <p>${product.price}</p>
+                <button onClick={() => dispatch(addToCart(product.id))}>
+                  Add to Cart{" "}
+                  <span role="img" aria-label="cart">
+                    🛒
+                  </span>
+                </button>
+              </div>
+            </article>
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+}
